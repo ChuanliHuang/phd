f43e28334409c89075657175994ebd4824e8762c
Chuanli Huange <s1943350@ed.ac.uk>
2022-10-03 14:40:04 +0100

    bub1-mNG in noc added error bar

--- plotting/220211/bub1_vs_time_noc.py
+++ plotting/220211/bub1_vs_time_noc.py
@@ -1,24 +1,28 @@
+from scipy.stats import sem
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
-file_name = '/Users/kikawaryoku/Library/CloudStorage/OneDrive-UniversityofEdinburgh/shugoshin/0_Image analysis/220208_Bub1-mNG_Mtw1-td_noc/quantification.xlsx'
+file_name = '/Users/kikawaryoku/Library/CloudStorage/OneDrive-UniversityofEdinburgh/shugoshin/1_Image analysis/220208_Bub1-mNG_Mtw1-td_noc/quantification.xlsx'
 df = pd.read_excel(file_name, sheet_name=0)
-
-for cell in df['cell'].unique():
-    selection = df['cell'] == cell
-    time = (df[selection]['time'] - 1) * 10 + 20
-    g_int = df[selection]['g_intensity']
-    plt.plot(time, g_int, '.', color='lightgreen', alpha=0.3)
 x = []
 y = []
+yerrs = []
 for time in df['time'].unique():
     selection = df['time'] == time
     x.append((time - 1) * 10 + 20)
-    g_int_median = df[selection]['g_intensity'].median()
-    y.append(g_int_median)
-plt.plot(x, y, color='tab:green', linewidth=2, label='median')
-plt.legend()
-plt.xlabel(r'time after $\alpha$ factor washout (min)')
-plt.ylabel('GFP intensity (a.u.)')
-plt.ylim(-100,400)
+    g_int_mean = df[selection]['g_intensity'].mean()
+    yerr = sem(df[selection]['g_intensity'])
+    yerrs.append(yerr)
+    y.append(g_int_mean)
+
+plt.errorbar(x, y, yerr=yerrs, color='tab:green', linewidth=2)
+
+# plt.legend(loc=1)
+# plt.xlabel(r'time after $\alpha$ factor washout (min)')
+# plt.xticks(np.arange(20, 160, step=10), ('20', '30', '40', '50', '60', '70', '80', '90', '100', '110', '120'))
+plt.xlabel(r'time after G1 release (min)')
+plt.ylabel('KT Bub1-mNG intensity (a.u.)')
+# plt.ylim(-15, 255)
+# plt.xlim(10, 170)
 plt.show()
